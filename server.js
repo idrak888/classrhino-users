e5d5a7778d55f672dfca89547c936870d2c655ec
Idrak Mustahsin <playbox8g@gmail.com>
2019-11-13 01:49:12 +0800

    Update server.js

--- server.js
+++ server.js
@@ -67,7 +67,7 @@ app.post("/teachers", (req, res) => {
 		gender: req.body.gender,
 		curriculums: req.body.curriculums,
 		subjects: req.body.subjects,
-		lanugages: req.body.languages,
+		languages: req.body.languages,
 		rate: req.body.rate,
 		location: {
 			country: req.body.country,
