b9231755f3b6b9549cc9fc61bbdd1118159e860c
Idrak Mustahsin <playbox8g@gmail.com>
2019-12-01 16:53:07 +0600

    Update server.js

--- server.js
+++ server.js
@@ -63,16 +63,16 @@ app.get("/teachers/search/:keywords", (req, res) => {
 
 app.get("/teachers/filter", (req, res) => {
 	var gender = req.body.gender;
-	Teacher.find().then(doc => {
-		var filteredTeachers = doc.filter(teacher => {
-			return (
-// 				(req.body.subject == '' || teacher.subjects.indexOf(req.body.subject) != -1) &&
-// 				(req.body.curriculum == '' || teacher.curriculums.indexOf(req.body.curriculum) != -1) &&
-				(gender == '' || teacher.gender == gender) 
-// 				(req.body.language == '' || teacher.languages.indexOf(req.body.language) != -1) &&
-// 				(req.body.country == '' || teacher.location.country.toLowerCase() == req.body.country.toLowerCase())
-			);
-		});
+ 	Teacher.find({gender}).then(doc => {
+// 		var filteredTeachers = doc.filter(teacher => {
+// 			return (
+// // 				(req.body.subject == '' || teacher.subjects.indexOf(req.body.subject) != -1) &&
+// // 				(req.body.curriculum == '' || teacher.curriculums.indexOf(req.body.curriculum) != -1) &&
+// 				(gender == '' || teacher.gender == gender) 
+// // 				(req.body.language == '' || teacher.languages.indexOf(req.body.language) != -1) &&
+// // 				(req.body.country == '' || teacher.location.country.toLowerCase() == req.body.country.toLowerCase())
+// 			);
+// 		});
 		
 		res.send(filteredTeachers);
     }).catch(e => {
